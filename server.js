aca900d001c02cbb10b836bcd5861b12d826d09c
pmacaluso3 <moxious.manoeuvre@gmail.com>
2021-04-23 09:31:08 -0400

    pokedex backend!

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const app = express()
+
+app.use(require('morgan')('tiny'))
+const routesReport = require('rowdy-logger').begin(app)
+
+app.use(express.json())
+app.use(require('cors')())
+
+const models = require('./models')
+
+app.get('/favPokemon', async (req, res) => {
+  try {
+    const favPokemon = await models.favPokemon.findAll()
+    res.json({ favPokemon })
+    
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
+})
+
+app.post('/favPokemon', async (req, res) => {
+  try {
+    const newFavPokemon = await models.favPokemon.create({
+      name: req.body.name
+    })
+    res.json({ newFavPokemon })
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
+})
+
+app.delete('/favPokemon/:id', async (req, res) => {
+  try {
+    const deleteResult = await models.favPokemon.destroy({
+      where: { id: req.params.id }
+    })
+    res.json({ deleteResult })
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
+})
+
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`server listening on ${PORT}`);
+  routesReport.print()
+})
