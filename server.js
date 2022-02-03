ade4b9b76532e491ef3f20a8cb28395867087dd8
mutotu <muttalip.aydemir@gmail.com>
2022-02-02 19:17:18 -0800

    transfering from enterprice

--- server.js
+++ server.js
@@ -1,49 +1,49 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.use(require('morgan')('tiny'))
-const routesReport = require('rowdy-logger').begin(app)
+app.use(require("morgan")("tiny"));
+const routesReport = require("rowdy-logger").begin(app);
 
-app.use(express.json())
-app.use(require('cors')())
+app.use(express.json());
+app.use(require("cors")());
 
-const models = require('./models')
+const models = require("./models");
 
-app.get('/favPokemon', async (req, res) => {
+app.get("/favPokemon", async (req, res) => {
   try {
-    const favPokemon = await models.favPokemon.findAll()
-    res.json({ favPokemon })
-    
+    const favPokemon = await models.favPokemon.findAll();
+    res.json({ favPokemon });
   } catch (error) {
-    res.status(400).json({ error: error.message })
+    res.status(400).json({ error: error.message });
   }
-})
+});
 
-app.post('/favPokemon', async (req, res) => {
+app.post("/favPokemon", async (req, res) => {
   try {
     const newFavPokemon = await models.favPokemon.create({
-      name: req.body.name
-    })
-    res.json({ newFavPokemon })
+      name: req.body.name,
+    });
+    console.log(newFavPokemon);
+    res.json({ newFavPokemon });
   } catch (error) {
-    res.status(400).json({ error: error.message })
+    res.status(400).json({ error: error.message });
   }
-})
+});
 
-app.delete('/favPokemon/:id', async (req, res) => {
+app.delete("/favPokemon/:name", async (req, res) => {
+  console.log(req.params.name);
   try {
     const deleteResult = await models.favPokemon.destroy({
-      where: { id: req.params.id }
-    })
-    res.json({ deleteResult })
+      where: { name: req.params.name },
+    });
+    res.json({ deleteResult });
   } catch (error) {
-    res.status(400).json({ error: error.message })
+    res.status(400).json({ error: error.message });
   }
-})
+});
 
-
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`server listening on ${PORT}`);
-  routesReport.print()
-})
+  routesReport.print();
+});
